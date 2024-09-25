ca366b8fe7241c97007232e78d7171d2b375204e
Am-Kitsune <105692663+AM-kitsune@users.noreply.github.com>
2024-09-25 08:35:02 +0700

    Update and rename PemilihanBilanganTernary13.java to PemilihanBilanganTernary14.java

--- PemilihanBilanganTernary14.java
+++ PemilihanBilanganTernary14.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PemilihanBilanganTernary13 {
+public class PemilihanBilanganTernary14 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukkan sebuah angka: ");
