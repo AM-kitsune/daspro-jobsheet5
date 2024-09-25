37bbbeb9d892c61a5c7fd5e4e57146cb9826dcf7
Am-Kitsune <105692663+AM-kitsune@users.noreply.github.com>
2024-09-25 07:50:22 +0700

    Add files via upload

---
+++ PemilihanHari13.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class PemilihanHari13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String dayName, dayType;
+
+        System.out.println("input day name: ");
+        dayName = sc.nextLine();
+        switch (dayName.toLowerCase()) {
+            case "monday" :
+            case "tuesday" :
+            case "wednesday" :
+            case "thursday" :
+            case "friday" :
+                dayType = "WeekDay";    
+                break;
+                case "saturday" :
+                case "sunday" :
+                dayType = "WeekEnd";
+                break;
+            default:
+                dayType = "Invalid day Name";
+}
+
+       System.err.println(dayName + " Is a " + dayType); 
+    }
+}
