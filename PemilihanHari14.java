104837202ac6b258f6e5d2858d80b2549434d1b8
Am-Kitsune <105692663+AM-kitsune@users.noreply.github.com>
2024-09-25 08:57:31 +0700

    Update and rename PemilihanHari13.java to PemilihanHari14.java

--- PemilihanHari14.java
+++ PemilihanHari14.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PemilihanHari13 {
+public class PemilihanHari14 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
